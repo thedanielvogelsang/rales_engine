fe0ebb47e951509eb2dd7e8a099ac94e37358d44
thedanielvogelsang <dr.vogelsang26@gmail.com>
2017-08-14 17:04:47 -0600

    adds index and show endpoints for merchant record

---
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe 'merchants api' do
+  it 'sends a list of merchants' do
+    create_list(:merchant, 3)
+
+    get '/api/v1/merchants'
+    assert_response :success
+    merchants = JSON.parse(response.body)
+    expect(merchants.count).to eq(3)
+  end
+
+  it 'can get one merchant by its id' do
+    id = create(:merchant).id
+    get "/api/v1/merchants/#{id}"
+    item = JSON.parse(response.body)
+    expect(response).to be_success
+    expect(item["id"]).to eq(id)
+  end
+end
