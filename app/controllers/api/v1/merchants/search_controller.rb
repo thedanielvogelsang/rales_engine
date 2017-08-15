181483a36a08c7265e343b9e93322163cad4ef41
Matt DeVoe <matthew.devoe@gmail.com>
2017-08-14 18:41:06 -0600

    merchants record endpoints test and build out completed

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -1,8 +1,11 @@
 class Api::V1::Merchants::SearchController < ApplicationController
 
   def show
-    
     render json: Merchant.find_by(params.keys.first.to_sym => params[params.keys.first])
+  end
+
+  def index
+    render json: Merchant.where(params.keys.first.to_sym => params[params.keys.first])
   end
 
 end
