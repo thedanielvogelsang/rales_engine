ba9bfb160d47fd3b8ba88d19ff60b7834fe2139d
Matt DeVoe <matthew.devoe@gmail.com>
2017-08-15 14:10:48 -0600

    FK relationship for cust/invo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170815195934) do
+ActiveRecord::Schema.define(version: 20170815200856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,11 +34,12 @@ ActiveRecord::Schema.define(version: 20170815195934) do
   end
 
   create_table "invoices", force: :cascade do |t|
-    t.integer "customer_id"
     t.integer "merchant_id"
     t.text "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "customer_id"
+    t.index ["customer_id"], name: "index_invoices_on_customer_id"
   end
 
   create_table "items", force: :cascade do |t|
