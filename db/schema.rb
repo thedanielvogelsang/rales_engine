12d84017d602e0d20191afbe2c3a377be8261cf2
thedanielvogelsang <dr.vogelsang26@gmail.com>
2017-08-15 08:04:38 -0600

    completes find endpoints for invoice_items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170815131921) do
+ActiveRecord::Schema.define(version: 20170815140215) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20170815131921) do
     t.bigint "invoice_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "quantity"
+    t.decimal "unit_price"
     t.index ["invoice_id"], name: "index_invoice_items_on_invoice_id"
     t.index ["item_id"], name: "index_invoice_items_on_item_id"
   end
--- spec/factories/invoice_items.rb
+++ spec/factories/invoice_items.rb
@@ -2,5 +2,7 @@ FactoryGirl.define do
   factory :invoice_item do
     item nil
     invoice nil
+    quantity 2
+    unit_price "9.99"
   end
 end
