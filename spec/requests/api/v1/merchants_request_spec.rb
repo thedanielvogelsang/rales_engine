50b527b97df46ea77f7edfc3f5f1cd11c1be58b1
Matt DeVoe <matthew.devoe@gmail.com>
2017-08-14 20:23:48 -0600

    minor changes

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -5,16 +5,23 @@ describe 'merchants api' do
     create_list(:merchant, 3)
 
     get '/api/v1/merchants'
+
     assert_response :success
+
     merchants = JSON.parse(response.body)
+
     expect(merchants.count).to eq(3)
   end
 
   it 'can get one merchant by its id' do
     id = create(:merchant).id
+
     get "/api/v1/merchants/#{id}"
+
     merchant = JSON.parse(response.body)
+
     expect(response).to be_success
+    
     expect(merchant["id"]).to eq(id)
   end
 end
