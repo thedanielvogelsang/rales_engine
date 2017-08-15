4df7de26cc6621f73ce09e94217206685b923c0b
Matt DeVoe <matthew.devoe@gmail.com>
2017-08-15 08:23:37 -0600

    find single customer by last name test passes

--- spec/requests/api/v1/customers/customers_search_spec.rb
+++ spec/requests/api/v1/customers/customers_search_spec.rb
@@ -24,4 +24,16 @@ describe 'Customer search API' do
 
     expect(customer["first_name"]).to eq(first_name)
   end
+
+  it "returns a single customer by last name " do
+    last_name = create(:customer).last_name
+
+    get "/api/v1/customers/find?last_name=#{last_name}"
+
+    expect(response).to be_success
+
+    customer = JSON.parse(response.body)
+
+    expect(customer["last_name"]).to eq(last_name)
+  end
 end
