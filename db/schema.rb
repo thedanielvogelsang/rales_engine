303f8765c0a7e4aeda797e8a4efc2ed56a4e5b37
thedanielvogelsang <dr.vogelsang26@gmail.com>
2017-08-14 20:55:34 -0600

    tests written and all item and item/id endpoints reached for records: invoice, items, invoice_items

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170815020325) do
+ActiveRecord::Schema.define(version: 20170815023147) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "invoice_items", force: :cascade do |t|
+    t.bigint "item_id"
+    t.bigint "invoice_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["invoice_id"], name: "index_invoice_items_on_invoice_id"
+    t.index ["item_id"], name: "index_invoice_items_on_item_id"
+  end
 
   create_table "invoices", force: :cascade do |t|
     t.integer "customer_id"
@@ -38,4 +47,6 @@ ActiveRecord::Schema.define(version: 20170815020325) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "invoice_items", "invoices"
+  add_foreign_key "invoice_items", "items"
 end
---
+++ spec/factories/invoice_items.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :invoice_item do
+    item nil
+    invoice nil
+  end
+end
