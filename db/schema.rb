53c6a5407c8a505f65ffdb9e97d78306b64d3d6d
Matt DeVoe <matthew.devoe@gmail.com>
2017-08-15 13:58:53 -0600

    added has many relationships to items and invoices

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20170815140215) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "customers", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "invoice_items", force: :cascade do |t|
     t.bigint "item_id"
@@ -42,11 +46,6 @@ ActiveRecord::Schema.define(version: 20170815140215) do
     t.text "description"
     t.decimal "unit_price"
     t.integer "merchant_id"
-
-  create_table "customers", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -57,10 +56,6 @@ ActiveRecord::Schema.define(version: 20170815140215) do
     t.datetime "updated_at", null: false
   end
 
-
-  add_foreign_key "invoice_items", "invoices"
-  add_foreign_key "invoice_items", "items"
-
   create_table "transactions", force: :cascade do |t|
     t.integer "invoice_id"
     t.string "credit_card_number"
@@ -70,5 +65,6 @@ ActiveRecord::Schema.define(version: 20170815140215) do
     t.datetime "updated_at", null: false
   end
 
-
+  add_foreign_key "invoice_items", "invoices"
+  add_foreign_key "invoice_items", "items"
 end
--- lib/tasks/import.rake
+++ lib/tasks/import.rake
@@ -1,12 +1,20 @@
+
 require 'csv'
 require 'open-uri'
+
+# Merchant.destroy_all
+# Customer.destroy_all
+# Invoice.destroy_all
+# Item.destroy_all
+# InvoiceItem.destroy_all
+# Transaction.destroy_all
 
 urls = {
   "Merchant" => "https://raw.githubusercontent.com/turingschool-examples/sales_engine/master/data/merchants.csv",
   "Customer" => "https://raw.githubusercontent.com/turingschool-examples/sales_engine/master/data/customers.csv",
-  "InvoiceItem" => "https://raw.githubusercontent.com/turingschool-examples/sales_engine/master/data/invoice_items.csv",
   "Invoice" => "https://raw.githubusercontent.com/turingschool-examples/sales_engine/master/data/invoices.csv",
   "Item" => "https://raw.githubusercontent.com/turingschool-examples/sales_engine/master/data/items.csv",
+  "InvoiceItem" => "https://raw.githubusercontent.com/turingschool-examples/sales_engine/master/data/invoice_items.csv",
   "Transaction" => "https://raw.githubusercontent.com/turingschool-examples/sales_engine/master/data/transactions.csv"
 }
 
