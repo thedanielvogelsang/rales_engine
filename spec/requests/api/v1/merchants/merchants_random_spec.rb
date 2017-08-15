242505896d08940a59c1b689b8fcb6aecffb1007
Matt DeVoe <matthew.devoe@gmail.com>
2017-08-15 07:39:57 -0600

    merchant random test passes

---
+++ spec/requests/api/v1/merchants/merchants_random_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe 'Merchant random API' do
+  it "returns a random merchant" do
+    merchant_1, merchant_2 = create_list(:merchant, 2)
+
+    get '/api/v1/merchants/random'
+
+    expect(response).to be_success
+
+    merchant = JSON.parse(response.body)
+
+    expect(merchant.first["id"]).to eq((merchant_1.id || merchant_2.id))
+  end
+end
