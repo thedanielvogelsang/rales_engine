7b6aef602df45d28ee82d881b35346bbf40db5b1
Matt DeVoe <matthew.devoe@gmail.com>
2017-08-15 14:12:59 -0600

    added merchant FK to invoices

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -3,4 +3,5 @@ class Invoice < ApplicationRecord
   has_many :items, through: :invoice_items
   has_many :transactions
   belongs_to :customer
+  belongs_to :merchant
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170815200856) do
+ActiveRecord::Schema.define(version: 20170815201114) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,12 +34,13 @@ ActiveRecord::Schema.define(version: 20170815200856) do
   end
 
   create_table "invoices", force: :cascade do |t|
-    t.integer "merchant_id"
     t.text "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "customer_id"
+    t.bigint "merchant_id"
     t.index ["customer_id"], name: "index_invoices_on_customer_id"
+    t.index ["merchant_id"], name: "index_invoices_on_merchant_id"
   end
 
   create_table "items", force: :cascade do |t|
