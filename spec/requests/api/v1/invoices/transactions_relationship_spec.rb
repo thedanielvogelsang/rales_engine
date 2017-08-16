a87ace3c54bf1738df2c1ee2a66bd160daabbac3
thedanielvogelsang <dr.vogelsang26@gmail.com>
2017-08-15 20:11:37 -0600

    completes invoice:transaction endpoint, test passes, code added to invoices/transactions_controller

--- spec/requests/api/v1/invoices/transactions_relationship_spec.rb
+++ spec/requests/api/v1/invoices/transactions_relationship_spec.rb
@@ -2,8 +2,13 @@ require 'rails_helper'
 
 describe 'invoices api transactions relationship' do
   it "returns an associated transaction given an invoice id" do
-    id = create(:invoice, :with_transactions).id
+    invoice = create(:invoice, :with_transactions)
+    id = invoice.id
+    transaction = invoice.transactions.first
     get "/api/v1/invoices/#{id}/transactions"
     assert_response :success
+    transactions = JSON.parse(response.body)
+    expect(transactions.count).to eq(3)
+    expect(transactions.first['id']).to eq(transaction.id)
   end
 end
