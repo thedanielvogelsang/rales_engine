bee905867905c17d48a3b6dfef7c918baffac9b1
thedanielvogelsang <dr.vogelsang26@gmail.com>
2017-08-15 20:15:18 -0600

    adds invoices:merchant relationship endpoint, completes test, adds code to invoices/transaction_controller.rb

--- spec/requests/api/v1/invoices/merchant_relationship_spec.rb
+++ spec/requests/api/v1/invoices/merchant_relationship_spec.rb
@@ -2,8 +2,12 @@ require 'rails_helper'
 
 describe 'invoices api merchant relationship' do
   it "returns associated merchant for an invoice id" do
-    id = create(:invoice).id
+    invoice = create(:invoice)
+    id = invoice.id
+    merchant_id = invoice.merchant.id
     get "/api/v1/invoices/#{id}/merchant"
     assert_response :success
+    merchant = JSON.parse(response.body)
+    expect(merchant['id']).to eq(merchant_id)
   end
 end
