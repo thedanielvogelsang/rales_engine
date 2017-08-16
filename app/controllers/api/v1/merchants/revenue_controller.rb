c492a7711cd62f647f913bdca5436e2ea07f61d3
Matt DeVoe <matthew.devoe@gmail.com>
2017-08-16 17:28:28 -0600

    BI problem complete, rails harness passes

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::Merchants::RevenueController < ApplicationController
 
   def show
     merchant = Merchant.find(params[:id])
+    render json: {"revenue": merchant.revenue_by_merchant}
   end
 
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,9 +3,10 @@ class Merchant < ApplicationRecord
   has_many :items
 
   def revenue_by_merchant
-    invoices
+    revenue = invoices
       .joins(:transactions, :invoice_items)
       .where(transactions: {result: "success"})
       .sum("invoice_items.unit_price * invoice_items.quantity")
+      revenue = '%.2f' % (revenue.to_i/100.0)
   end
 end
