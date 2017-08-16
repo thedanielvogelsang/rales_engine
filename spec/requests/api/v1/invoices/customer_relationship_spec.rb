e3aec06de752e4e117ca5abbf7052c3c5b265cf4
thedanielvogelsang <dr.vogelsang26@gmail.com>
2017-08-15 20:18:04 -0600

    adds invoices:customer relationship endpoint, creates test in invoices/customer_relationship_spec.rb, adds code to invoices/customer_controller.rb

--- spec/requests/api/v1/invoices/customer_relationship_spec.rb
+++ spec/requests/api/v1/invoices/customer_relationship_spec.rb
@@ -2,9 +2,12 @@ require 'rails_helper'
 
 describe 'invoices api customer relationship' do
   it "returns the associated customer for an invoice id" do
-    id = create(:invoice).id
-    byebug
+    invoice = create(:invoice)
+    id = invoice.id
+    customer_id = invoice.customer.id
     get "/api/v1/invoices/#{id}/customer"
     assert_response :success
+    customer = JSON.parse(response.body)
+    expect(customer['id']).to eq(customer_id)
   end
 end
