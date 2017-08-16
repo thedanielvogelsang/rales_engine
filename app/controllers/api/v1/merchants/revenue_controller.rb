c783844e1ee65e3c12b8098088aa59276b132352
Matt DeVoe <matthew.devoe@gmail.com>
2017-08-16 16:52:38 -0600

    active record added for finding total revenue by merchant

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::Merchants::RevenueController < ApplicationController
 
   def show
-
+    merchant = Merchant.find(params[:id])
   end
 
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,11 @@
 class Merchant < ApplicationRecord
   has_many :invoices
   has_many :items
+
+  def revenue_by_merchant
+    invoices
+      .joins(:transactions, :invoice_items)
+      .where(transactions: {result: "success"})
+      .sum("invoice_items.unit_price * invoice_items.quantity")
+  end
 end
