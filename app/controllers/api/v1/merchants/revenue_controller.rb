8a0060a070dbd753dfeb93104d185dc62e8698e6
Matt DeVoe <matthew.devoe@gmail.com>
2017-08-17 16:46:03 -0600

    slight updates

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -2,9 +2,8 @@ class Api::V1::Merchants::RevenueController < ApplicationController
 
   def show
     merchant = Merchant.find(params[:id]) if params[:id]
-    date = search_params[:date]
     if search_params[:date] && search_params[:id].nil?
-      render json: {"total_revenue": Invoice.revenue_by_date(date)}
+      render json: {"total_revenue": Invoice.revenue_by_date(search_params[:date])}
     elsif search_params[:id] && search_params[:date].nil?
       render json: {"revenue": merchant.revenue_by_merchant}
     else
@@ -15,6 +14,8 @@ class Api::V1::Merchants::RevenueController < ApplicationController
   def index
     render json: Merchant.most_revenue(search_params[:quantity])
   end
+
+  private
 
   def search_params
     params.permit(:id, :date, :quantity)
