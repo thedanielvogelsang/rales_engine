4313eda725313361d49cb466d696fab06bc93b9a
Matt DeVoe <matthew.devoe@gmail.com>
2017-08-17 08:10:18 -0600

    fixed 'end' error in merchant.rb file

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -17,6 +17,7 @@ class Merchant < ApplicationRecord
       .where(transactions: {result: "success"})
       .sum("invoice_items.unit_price * invoice_items.quantity")
     revenue = '%.2f' % (revenue.to_i/100.0)
+  end
 
   def customers
     Customer.joins(:invoices).where(invoices: {merchant_id: self.id}).distinct
