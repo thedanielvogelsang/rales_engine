98fb3913c7fc511f9a8240c9f56c78a5e75cce42
Matt DeVoe <matthew.devoe@gmail.com>
2017-08-17 14:48:38 -0600

    best day logic completed, spec harness passes, serializer in place

--- app/controllers/api/v1/items/bestday_controller.rb
+++ app/controllers/api/v1/items/bestday_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::Items::BestdayController < ApplicationController
 
   def show
-
+    render json: Item.find(params[:id]).best_day, serializer: BestdaySerializer
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,11 @@ class Item < ApplicationRecord
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
   belongs_to :merchant
+
+  def best_day
+    invoices.joins(:invoice_items)
+      .group(:id)
+      .order('sum(invoice_items.quantity) DESC, invoices.created_at DESC')
+      .first
+  end
 end
