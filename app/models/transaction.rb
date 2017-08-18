f29355cddbd056aad5fd86964849e8adacde383c
Matt DeVoe <matthew.devoe@gmail.com>
2017-08-18 08:23:16 -0600

    pushed logic down on customers::transactionscontroller

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,4 +3,9 @@ class Transaction < ApplicationRecord
 
   scope :successful, -> {where(result: 'success')}
   scope :not_successful, -> { where(result: 'failed')}
+
+  def self.customer_transactions(id)
+    joins(:invoice)
+    .merge(Invoice.where(customer_id: id))
+  end
 end
